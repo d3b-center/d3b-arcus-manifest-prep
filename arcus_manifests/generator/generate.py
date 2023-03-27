ce2301a703c43ee01e9836aaa0db003a217abecc
chris-s-friedman <chris-s-friedman@users.noreply.github.com>
2023-03-27 14:36:09 -0400

    ✨ add functionality to generate participant crosswalk

--- arcus_manifests/generator/generate.py
+++ arcus_manifests/generator/generate.py
@@ -1,5 +1,8 @@
 from arcus_manifests.common.constants import ALL_GENERATOR_LIST
 from arcus_manifests.generator.file.build_manifest import build_file_table
+from arcus_manifests.generator.participant_crosswalk.build_manifest import (
+    build_participant_crosswalk_table,
+)
 
 import pandas as pd
 from d3b_cavatica_tools.utils.logging import get_logger
@@ -7,16 +10,17 @@ from d3b_cavatica_tools.utils.logging import get_logger
 # from arcus_manifests.generator.participant.build_manifest import (
 #     build_participant_table,
 # )
-# from arcus_manifests.generator.participant_crosswalk.build_manifest import (
-#     build_participant_crosswalk_table,
-# )
 
 
 logger = get_logger(__name__, testing_mode=False)
 
 
 def generate_submission_package(
-    postgres_connection_url, submission_package_dir, seed_file, generator_list
+    postgres_connection_url,
+    submission_package_dir,
+    seed_file,
+    mrn_map_file,
+    generator_list,
 ):
     if "all" in generator_list:
         logger.info("Generating all manifests in submission packet")
@@ -29,21 +33,26 @@ def generate_submission_package(
 
     logger.info("Reading seed file")
     file_sample_participant_map = pd.read_csv(seed_file)
-    # participant_list = (
-    #     file_sample_participant_map["participant_id"]
-    #     .drop_duplicates()
-    #     .to_list()
-    # )
+    mrn_map = pd.read_csv(mrn_map_file)[["research_id", "mrn"]]
+    participant_list = (
+        file_sample_participant_map["research_id"].drop_duplicates().to_list()
+    )
     # sample_list = (
     #     file_sample_participant_map["sample_id"].drop_duplicates().to_list()
     # )
     file_list = (
-        file_sample_participant_map["file_id"].drop_duplicates().to_list()
+        file_sample_participant_map["genomic_file_id"]
+        .drop_duplicates()
+        .to_list()
     )
     # Save the seed file
     file_sample_participant_map.to_csv(
         f"{submission_package_dir}/file_sample_participant_map.csv", index=False
     )
+    if "participant_crosswalk" in generator_list:
+        build_participant_crosswalk_table(
+            participant_list, mrn_map, submission_package_dir
+        )
 
     if "file" in generator_list:
         build_file_table(
---
+++ arcus_manifests/generator/participant_crosswalk/build_manifest.py
@@ -0,0 +1,58 @@
+from arcus_manifests.common.standard import order_columns
+
+import pandas as pd
+from d3b_cavatica_tools.utils.logging import get_logger
+
+logger = get_logger(__name__, testing_mode=False)
+
+
+def validate_mrn(mrn):
+    """Validate MRNs
+
+    Validates that the MRN can be coerced to be integer and that the mrn is no
+    greater than 8 characters long, Returned MRN is zero-padded to be 8
+    characters long
+
+    :param mrn: an MRN
+    :type mrn: str or int
+    :return: zero-padded, 8 character-long MRN
+    :rtype: str
+    """
+    assert (
+        isinstance(mrn, int) or mrn.isdigit()
+    ), "MRN must be coercible to integer"
+    assert len(str(mrn)) > 8, "MRNs cannot be longer than 8 characters"
+    return f"{int(mrn):08}"
+
+
+def build_participant_crosswalk_table(
+    participant_list, mrn_map, submission_package_dir
+):
+    logger.info("Building Participant Crosswalk Table")
+    column_order = [
+        "local_id_type",
+        "local_participant_id",
+        "auth_id_type",
+        "auth_participant_id",
+    ]
+    participant_mrn_map = mrn_map[mrn_map["research_id"].isin(participant_list)]
+    participant_mrn_map["mrn"] = participant_mrn_map["mrn"].apply(validate_mrn)
+    participant_mrn_map = participant_mrn_map.rename(
+        columns={
+            "research_id": "local_participant_id",
+            "mrn": "auth_participant_id",
+        }
+    )
+    participant_mrn_map["local_id_type"] = "local"
+    participant_mrn_map["auth_id_type"] = "chop"
+    crosswalk_table = order_columns(
+        participant_mrn_map, column_order
+    ).sort_values("local_participant_id")
+    logger.info("Saving participant_crosswalk to file")
+    crosswalk_table.to_csv(
+        f"{submission_package_dir}/participant-crosswalk.txt",
+        sep="\t",
+        header=False,
+        index=False,
+    )
+    return crosswalk_table
